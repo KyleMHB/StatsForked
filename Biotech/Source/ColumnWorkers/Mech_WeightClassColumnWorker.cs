63b7f1ec5ff91bd8ab263fde67740496ae4f8372
Dmitry "Azzkiy <gwmdmitry@gmail.com>
2025-07-13 09:21:32 +0300

    1.6

--- Biotech/Source/ColumnWorkers/Mech_WeightClassColumnWorker.cs
+++ Biotech/Source/ColumnWorkers/Mech_WeightClassColumnWorker.cs
@@ -1,41 +1,14 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using RimWorld;
-using Stats.Widgets;
-using Verse;
+﻿using Verse;
 
 namespace Stats.Compat.Biotech;
 
-public sealed class Mech_WeightClassColumnWorker : ColumnWorker<ThingAlike>
+public sealed class Mech_WeightClassColumnWorker : DefColumnWorker<ThingAlike, MechWeightClassDef>
 {
-    public Mech_WeightClassColumnWorker(ColumnDef columnDef) : base(columnDef, ColumnCellStyle.String)
+    public Mech_WeightClassColumnWorker(ColumnDef columnDef) : base(columnDef)
     {
     }
-    public override Widget? GetTableCellWidget(ThingAlike thing)
+    protected override MechWeightClassDef GetValue(ThingAlike thing)
     {
-        var raceProps = thing.Def.race;
-
-        if (raceProps == null)
-        {
-            return null;
-        }
-
-        return new Label(raceProps.mechWeightClass.ToStringHuman().CapitalizeFirst());
-    }
-    public override IEnumerable<ObjectProp> GetObjectProps(IEnumerable<ThingAlike> tableRecords)
-    {
-        var filterOptions = tableRecords
-            .Select(thing => thing.Def.race?.mechWeightClass)
-            .Distinct()
-            .OrderBy(option => option)
-            .Select<MechWeightClass?, NTMFilterOption<MechWeightClass?>>(
-                mechWeightClass => mechWeightClass == null ? new() : new(mechWeightClass, ((MechWeightClass)mechWeightClass).ToStringHuman().CapitalizeFirst())
-            );
-
-        yield return new(ColumnDef.Title, Make.OTMFilter<ThingAlike, MechWeightClass?>(thing => thing.Def.race?.mechWeightClass, filterOptions));
-    }
-    public override int Compare(ThingAlike thing1, ThingAlike thing2)
-    {
-        return Comparer<MechWeightClass?>.Default.Compare(thing1.Def.race?.mechWeightClass, thing2.Def.race?.mechWeightClass);
+        return thing.Def.race.mechWeightClass;
     }
 }
