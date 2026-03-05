cd578fdf528617680a143d7609683c134ed88ccd
Dmitry "Azzkiy <gwmdmitry@gmail.com>
2026-03-05 23:06:18 +0300

    WIP

--- Core/Source/ColumnWorkers/BuildableDef/StatColumnWorker.cs
+++ Core/Source/ColumnWorkers/BuildableDef/StatColumnWorker.cs
@@ -1,138 +1,146 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text.RegularExpressions;
+﻿using System.Text.RegularExpressions;
 using RimWorld;
-using Stats.ColumnWorkers;
-using Stats.Objects.ThingDef;
-using Stats.ObjectTable;
+using Stats.FilterWidgets;
+using Stats.TableCells;
 using Stats.TableWorkers;
-using Stats.Widgets;
+using UnityEngine;
 using Verse;
 
 namespace Stats.ColumnWorkers.BuildableDef;
 
-public class StatColumnWorker : ColumnWorker, IColumnWorker<VirtualThing>, IColumnWorker<Thing>
+public class StatColumnWorker(StatColumnDef columnDef) : ColumnWorker<DefBasedObject, StatColumnWorker.StatCell>
 {
-    public static readonly Regex NumberRegex = new(@"(-?[0-9]+\.?[0-9]*).*", RegexOptions.Compiled);
+    public override ColumnDef Def => columnDef;
+
+    private static readonly Regex _numberRegex = new(@"(-?[0-9]+\.?[0-9]*).*", RegexOptions.Compiled);
     private const ToStringNumberSense _ToStringNumberSense = ToStringNumberSense.Absolute;
-    protected StatDef Stat { get; }
-    public StatColumnWorker(StatColumnDef columnDef) : base(columnDef, IColumnWorker.CellStyleType.Number)
-    {
-        Stat = columnDef.stat;
-    }
-    protected virtual string? GetStatDrawEntryLabel(StatDef stat, float value, ToStringNumberSense numberSense, StatRequest statRequest)
-    {
-        return stat.Worker.GetStatDrawEntryLabel(stat, value, _ToStringNumberSense, statRequest);
-    }
-    public ObjectTableWidget.Cell GetCell(VirtualThing thing)
-    {
-        return new Cell(thing, this);
-    }
-    public IEnumerable<ObjectTableWidget.ColumnPart> GetObjectProps(TableWorker<VirtualThing> _)
-    {
-        yield return new(Def.Title, new NumberFilter(cell => ((Cell)cell).ValueDisplayed));
-    }
-    public ObjectTableWidget.Cell GetCell(Thing thing)
-    {
-        throw new NotImplementedException();
-    }
-    public IEnumerable<ObjectTableWidget.ColumnPart> GetObjectProps(TableWorker<Thing> _)
-    {
-        throw new NotImplementedException();
-    }
+    private readonly StatDef _stat = columnDef.stat;
 
-    private class Cell : ObjectTableWidget.WidgetCell, ObjectTableWidget.Cell.IRefreshable
+    protected override StatCell MakeCell(DefBasedObject @object)
     {
-        protected override Widget? Widget { get; set; }
-        public float ValueRaw { get; private set; }
-        public decimal ValueDisplayed { get; private set; }
-        private readonly bool IsValidForThing;
-        private readonly StatColumnWorker Column;
-        private readonly StatDef Stat;
-        private readonly StatWorker StatWorker;
-        private readonly VirtualThing Thing;
-        public Cell(VirtualThing thing, StatColumnWorker column)
+        StatRequest statRequest;
+        if (@object.Thing != null)
         {
-            Thing = thing;
-            Column = column;
-            Stat = column.Stat;
-            StatWorker = column.Stat.Worker;
-            var statRequest = thing.Thing != null
-                ? StatRequest.For(thing.Thing)
-                : StatRequest.For(thing.Def, thing.StuffDef);
-            IsValidForThing = StatWorker.ShouldShowFor(statRequest);
-
-            if (IsValidForThing)
-            {
-                var statValue = StatWorker.GetValue(statRequest);
-
-                Refresh_Int(statRequest, statValue);
-            }
-
-            //if (thing.Thing != null && column.Stat.immutable == false && IsValidForThing)
-            //{
-            //}
+            statRequest = StatRequest.For(@object.Thing);
         }
-        private void Refresh_Int(StatRequest statRequest, float statValue)
+        else if (@object.Def is Verse.BuildableDef buildableDef)
         {
-            if (IsValidForThing && statValue != 0f)
-            {
-                var cellText = Column.GetStatDrawEntryLabel(Stat, statValue, _ToStringNumberSense, statRequest);
-
-                if (cellText != null)
-                {
-                    var displayedValue = 0m;
-                    var match = NumberRegex.Match(cellText);
-
-                    if (match.Success)
-                    {
-                        displayedValue = decimal.Parse(match.Groups[1].Captures[0].Value);
-                    }
-
-                    var tooltip = StatWorker.GetExplanationFull(statRequest, _ToStringNumberSense, statValue);
-
-                    Widget widget = new Label(cellText)
-                    .PaddingAbs(ObjectTableWidget.CellPadHor, ObjectTableWidget.CellPadVer);
-
-                    if (tooltip != null)
-                    {
-                        widget = widget.Tooltip(tooltip);
-                    }
-
-                    Widget = widget;
-                    ValueRaw = statValue;
-                    ValueDisplayed = displayedValue;
-                }
-            }
-            else
-            {
-                Widget = null;
-                ValueRaw = 0f;
-                ValueDisplayed = 0m;
-            }
+            statRequest = StatRequest.For(buildableDef, @object.StuffDef);
         }
-        public bool Refresh()
+        else
         {
-            if (IsValidForThing)
+            return default;
+        }
+
+        if (_stat.Worker.ShouldShowFor(statRequest))
+        {
+            float statValue = _stat.Worker.GetValue(statRequest);
+
+            return new StatCell(statValue, statRequest, _stat);
+        }
+
+        return default;
+    }
+
+    protected override StatCell RefreshCell(StatCell cell, out bool wasStale)
+    {
+        float newStatValue = _stat.Worker.GetValue(cell.StatRequest);
+
+        if (newStatValue != cell.ValueRaw)
+        {
+            wasStale = true;
+            return new StatCell(newStatValue, cell.StatRequest, _stat);
+        }
+
+        wasStale = false;
+        return cell;
+    }
+
+    public override TableCellDescriptor GetCellDescriptor(TableWorker tableWorker)
+    {
+        FilterWidget valueFieldFilter = new NumberFilter((int row) => this[row].Value);
+        int Compare(int row1, int row2) => this[row1].Value.CompareTo(this[row2].Value);
+        TableCellFieldDescriptor valueField = new(Def.Title, valueFieldFilter, Compare);
+
+        return new TableCellDescriptor(TableCellStyleType.Number, [valueField]);
+    }
+
+    public override void NotifyRowRemoved(int row)
+    {
+        _tooltipOwner = default;
+        // TODO:
+        //
+        // Set _tooltip to null also?
+        // Although cell removal code is already a bit heavier than i wanted it to be,
+        // and i don't think a single string, that just hangs around in memory, is that big of an issue.
+
+        base.NotifyRowRemoved(row);
+    }
+
+    // TODO:
+    //
+    // Tooltip may become stale despite the cell's value being fresh.
+    // This happens when we do not refresh a cell because its value haven't changed.
+    // But the way we got to this value may have changed, and tooltip displays that info.
+    private static StatRequest _tooltipOwner;
+    private static TipSignal? _tooltip;
+
+    public readonly struct StatCell : ITableCell
+    {
+        public float Width { get; }
+        public bool IsRefreshable => StatRequest.Thing != null;
+        public readonly decimal Value;
+        public readonly float ValueRaw;
+        public readonly StatRequest StatRequest;
+
+        private readonly StatDef? _stat;
+        private readonly string? _text;
+
+        public StatCell(float statValue, StatRequest statRequest, StatDef stat)
+        {
+            _stat = stat;
+            StatRequest = statRequest;
+            if (statValue != 0f)
             {
-                var statRequest = Thing.Thing != null
-                    ? StatRequest.For(Thing.Thing)
-                    : StatRequest.For(Thing.Def, Thing.StuffDef);
-                var statValue = StatWorker.GetValue(statRequest);
-
-                if (ValueRaw != statValue)
+                ValueRaw = statValue;
+                _text = stat.Worker.GetStatDrawEntryLabel(stat, statValue, _ToStringNumberSense, statRequest);
+                Match match = _numberRegex.Match(_text);
+                if (match.Success)
                 {
-                    Refresh_Int(statRequest, statValue);
-
-                    return true;
+                    Value = decimal.Parse(match.Groups[1].Captures[0].Value);
                 }
             }
 
-            return false;
+            // Drop tooltip on refresh.
+            if (_tooltip != null && _tooltipOwner == statRequest)
+            {
+                _tooltip = null;
+            }
         }
-        public override int CompareTo(ObjectTableWidget.Cell cell)
+
+        public void Draw(Rect rect)
         {
-            return ValueDisplayed.CompareTo(((Cell)cell).ValueDisplayed);
+            if (Value != 0m && Event.current.type == EventType.Repaint)
+            {
+                if (Mouse.IsOver(rect))
+                {
+                    if (_tooltip == null || _tooltipOwner != StatRequest)
+                    {
+                        _tooltip = _stat.Worker.GetExplanationFull(StatRequest, _ToStringNumberSense, ValueRaw);
+                        _tooltipOwner = StatRequest;
+                    }
+                    TooltipHandler.TipRegion(rect, _tooltip.Value);
+                }
+
+                rect = rect.ContractedByObjectTableCellPadding();
+
+                TextAnchor textAnchor = Text.Anchor;
+                Text.Anchor = (TextAnchor)TableCellStyleType.Number;
+
+                Verse.Widgets.Label(rect, _text);
+
+                Text.Anchor = textAnchor;
+            }
         }
     }
 }
---
+++ Core/Source/ColumnWorkers/Def/ModContentPackColumnWorker.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Stats.FilterWidgets;
+using Stats.TableCells;
+using Stats.TableWorkers;
+using UnityEngine;
+using Verse;
+
+namespace Stats.ColumnWorkers.Def;
+
+public sealed class ModContentPackColumnWorker(ColumnDef columnDef) : ColumnWorker<DefBasedObject, ModContentPackColumnWorker.ModContentPackCell>
+{
+    public override ColumnDef Def => columnDef;
+
+    protected override ModContentPackCell MakeCell(DefBasedObject @object)
+    {
+        ModContentPack? modContentPack = @object.Def.modContentPack;
+
+        if (modContentPack != null)
+        {
+            return new ModContentPackCell(modContentPack);
+        }
+
+        return default;
+    }
+
+    public override TableCellDescriptor GetCellDescriptor(TableWorker tableWorker)
+    {
+        IEnumerable<NTMFilterOption<ModContentPack?>> valueFieldFilterOptions = ((IRefRecordsProvider<Verse.Def>)tableWorker).Records
+            .Select(def => def.modContentPack)
+            .Distinct()
+            .OrderBy(mod => mod?.Name)
+            .Select<ModContentPack?, NTMFilterOption<ModContentPack?>>(
+                mod => mod == null ? new() : new(mod, mod.Name, null, mod.PackageIdPlayerFacing)
+            );
+        FilterWidget valueFieldFilter = new OTMFilter<ModContentPack?>((int row) => this[row].Mod, valueFieldFilterOptions);
+        int Compare(int row1, int row2) => this[row1].ModName.CompareTo(this[row2].ModName);
+        TableCellFieldDescriptor valueField = new(Def.Title, valueFieldFilter, Compare);
+
+        return new TableCellDescriptor(TableCellStyleType.String, [valueField]);
+    }
+
+    public readonly struct ModContentPackCell : ITableCell
+    {
+        public float Width { get; }
+        public bool IsRefreshable => false;
+        public readonly ModContentPack? Mod;
+        public readonly string ModName = "";
+        private readonly TipSignal _tooltip;
+
+        public ModContentPackCell(ModContentPack mod)
+        {
+            Mod = mod;
+            ModName = mod.Name;
+            Width = Text.CalcSize(ModName).x;
+            _tooltip = mod.PackageIdPlayerFacing;
+        }
+
+        public void Draw(Rect rect)
+        {
+            if (Mod != null && Event.current.type == EventType.Repaint)
+            {
+                TooltipHandler.TipRegion(rect, _tooltip);
+
+                rect = rect.ContractedByObjectTableCellPadding();
+
+                TextAnchor textAnchor = Text.Anchor;
+                Text.Anchor = (TextAnchor)TableCellStyleType.String;
+
+                Verse.Widgets.Label(rect, ModName);
+
+                Text.Anchor = textAnchor;
+            }
+        }
+    }
+}
